f31f4153be92e5cc77dbaf2fb315d0e23eed27c1
Yismen Jorge <yismen.jorge@gmail.com>
2023-09-03 14:31:25 -0400

    Fix alignment issue in MyTickets view

--- resources/views/livewire/ticket/index.blade.php
+++ resources/views/livewire/ticket/index.blade.php
@@ -1,14 +1,10 @@
 <div>
     <livewire:support::ticket.detail />
-    <livewire:support::ticket.form />
-    <div class="d-flex justify-content-between">
-        <div class="" style="flex: 1;">
-            <div class="card ">
-                <div class="card-body text-black" :key="time()">
-                    <h3 class="border-bottom pb-3">My Tickets</h3>
-                    <livewire:support::ticket.table />
-                </div>
-            </div>
+    <livewire:support::ticket.form />\
+    <div class="card ">
+        <div class="card-body text-black" :key="time()">
+            <h3 class="border-bottom pb-3">My Tickets</h3>
+            <livewire:support::ticket.table />
         </div>
     </div>
 
